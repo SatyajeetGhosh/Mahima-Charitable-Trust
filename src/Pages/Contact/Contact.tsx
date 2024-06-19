730dafb1f80872ca4f2814029c29c45b2192a5fb
SatyajeetGhosh <satyajeetghosh18@gmail.com>
2024-06-20 01:05:10 +0530

    added structure of entire contact form section

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
+import { FaPhoneAlt, FaGlobeAfrica } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
@@ -42,7 +45,7 @@ const Contact = () => {
       </section>
       <section className="contact-section">
         <div className="contact-form">
-          <h2>Get In Touch</h2>
+          <h2 className="contact-sub-heading">Get In Touch</h2>
           <Form>
             <div>
               <Form.Item
@@ -78,9 +81,31 @@ const Contact = () => {
           </Form>
         </div>
         <div className="trust-details">
-          <h2>Contact Us</h2>
+          <h2 className="contact-sub-heading">Contact Us</h2>
           <div>
-            
+            <div className="google-maps-container">
+              <iframe
+                title="Mahima Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.458404975592!2d77.6023886736924!3d13.006454414115959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1656ac5424d1%3A0x243cf79b92636f20!2sMahima%20Charitable%20Trust!5e0!3m2!1sen!2sin!4v1691475373296!5m2!1sen!2sin"
+                loading="lazy"
+                style={{ border: 0 }}
+                allowFullScreen
+              ></iframe>
+            </div>
+            <div className="mahima-details-container">
+              <div className="mahima-details">
+                <FaPhoneAlt />
+                <div>+91 9535447505</div>
+              </div>
+              <div className="mahima-details">
+                <MdEmail />
+                <div>mctbangalore@gmail.com</div>
+              </div>
+              <div className="mahima-details">
+                <FaGlobeAfrica />
+                <div>mahimaCharitabletrust.org</div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
